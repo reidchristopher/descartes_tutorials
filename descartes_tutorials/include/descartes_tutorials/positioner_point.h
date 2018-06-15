753d0afd2a4375a3208629de48c9bc75f4f641b3
mripperger <michael.ripperger@swri.org>
2018-06-15 18:01:44 -0500

    Added means by which to specify the symmetric tolerances to discretize about for the positioner and tool

--- descartes_tutorials/include/descartes_tutorials/positioner_point.h
+++ descartes_tutorials/include/descartes_tutorials/positioner_point.h
@@ -6,7 +6,18 @@
 class PositionerPoint : public descartes_core::TrajectoryPt
 {
 public:
-  PositionerPoint(const Eigen::Affine3d& turn_table, const Eigen::Affine3d& pt, double turn_table_disc, double pt_disc,
+  PositionerPoint(const Eigen::Affine3d& turn_table,
+                  const Eigen::Affine3d& pt,
+                  double turn_table_disc,
+                  double turn_table_sym_tol,
+                  double pt_disc,
+                  double pt_sym_tol,
+                  descartes_core::TimingConstraint timing);
+
+  PositionerPoint(const Eigen::Affine3d& turn_table,
+                  const Eigen::Affine3d& pt,
+                  double turn_table_disc,
+                  double pt_disc,
                   descartes_core::TimingConstraint timing);
 
   // TrajectoryPt interface
@@ -23,7 +34,7 @@ public:
 
 private:
   Eigen::Affine3d turn_table_, pt_;
-  double table_disc_, pt_disc_;
+  double table_disc_, table_sym_tol_, pt_disc_, pt_sym_tol_;
 };
 
 #endif // POSITIONER_POINT_H
--- descartes_tutorials/src/positioner_point.cpp
+++ descartes_tutorials/src/positioner_point.cpp
@@ -1,7 +1,34 @@
 #include "descartes_tutorials/positioner_point.h"
 
-PositionerPoint::PositionerPoint(const Eigen::Affine3d &turn_table, const Eigen::Affine3d &pt, double turn_table_disc, double pt_disc, descartes_core::TimingConstraint timing)
-  : TrajectoryPt(timing), turn_table_(turn_table), pt_(pt), table_disc_(turn_table_disc), pt_disc_(pt_disc)
+PositionerPoint::PositionerPoint(const Eigen::Affine3d &turn_table,
+                                 const Eigen::Affine3d &pt,
+                                 double turn_table_disc,
+                                 double turn_table_sym_tol,
+                                 double pt_disc,
+                                 double pt_sym_tol,
+                                 descartes_core::TimingConstraint timing)
+  : TrajectoryPt(timing),
+    turn_table_(turn_table),
+    pt_(pt),
+    table_disc_(turn_table_disc),
+    table_sym_tol_(turn_table_sym_tol),
+    pt_disc_(pt_disc),
+    pt_sym_tol_(pt_sym_tol)
+{
+}
+
+PositionerPoint::PositionerPoint(const Eigen::Affine3d &turn_table,
+                                 const Eigen::Affine3d &pt,
+                                 double turn_table_disc,
+                                 double pt_disc,
+                                 descartes_core::TimingConstraint timing)
+  : TrajectoryPt(timing),
+    turn_table_(turn_table),
+    pt_(pt),
+    table_disc_(turn_table_disc),
+    table_sym_tol_(M_PI),
+    pt_disc_(pt_disc),
+    pt_sym_tol_(M_PI)
 {
 }
 
@@ -33,9 +60,9 @@ bool PositionerPoint::getNominalJointPose(const std::vector<double> &seed_state,
 void PositionerPoint::getJointPoses(const descartes_core::RobotModel &model, std::vector<std::vector<double> > &joint_poses) const
 {
   std::vector<std::vector<double>> results;
-  for (double table = -M_PI; table < M_PI; table += table_disc_)
+  for (double table = -table_sym_tol_; table < table_sym_tol_; table += table_disc_)
   {
-    for (double angle = -M_PI; angle < M_PI; angle += 0.25)
+    for (double angle = -pt_sym_tol_; angle < pt_sym_tol_; angle += pt_disc_)
     {
       Eigen::Affine3d pose = turn_table_ * Eigen::AngleAxisd(table, Eigen::Vector3d::UnitZ()) * pt_ * Eigen::AngleAxisd(angle, Eigen::Vector3d::UnitZ());
       model.getAllIK(pose, results);
